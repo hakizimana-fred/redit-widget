32ae6484620dde260442ed47a00e98da3f6bf431
hakifred <hakifred20@gmail.com>
2020-10-14 20:50:11 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const WidgetDivs = document.querySelectorAll('.reddit_widget')
+
+WidgetDivs.forEach(Div => {
+  const dataAttributes = Div.dataset.subreddit
+  ReactDOM.render(
+    
+    <React.StrictMode>
+      <App  dataAttributes={ dataAttributes }/>
+    </React.StrictMode>,
+    Div
+  );
+})
+
